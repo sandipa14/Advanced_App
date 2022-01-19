ddbff6bc3e623b9ea8be1d0ff83cbc8763a0ed42
Sandipa Roy <sandipa1407@gmail.com>
2022-01-19 21:11:27 +0530

    initial commit

---
+++ app/src/main/java/com/cscr/cscr/my/MainSportsbuttonActivity.java
@@ -0,0 +1,41 @@
+package com.cscr.cscr.my;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainSportsbuttonActivity extends AppCompatActivity {
+    Button comSports, cricket;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sportsbutton);
+        comSports = findViewById(R.id.buttonCSports);
+        cricket = findViewById(R.id.buttonCricketRelated);
+        comSports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("typeOfList", 999);
+                bundle.putInt("Flag",11);
+                Intent i= new Intent(MainSportsbuttonActivity.this,Main3Activity.class);
+                i.putExtras(bundle);
+                startActivity(i);
+            }
+        });
+        cricket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("typeOfList", 999);
+                bundle.putInt("Flag",12);
+                Intent i= new Intent(MainSportsbuttonActivity.this,Main3Activity.class);
+                i.putExtras(bundle);
+                startActivity(i);
+            }
+        });
+    }
+}
